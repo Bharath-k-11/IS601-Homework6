6fa7401e4cbfee15b2b40b9ecf526235c4f7ec35
Kallam Jai Bharath Reddy <bharathreddy@Kallams-MacBook-Air.local>
2025-02-25 20:32:58 -0500

    worked on coverage

--- app/commands/addcommand.py
+++ app/commands/addcommand.py
@@ -3,11 +3,9 @@ from app.calculator import Calculator
 from app.commands import Command
 
 class Add(Command):
-    def __init__(self, command_handler):
-        self.command_handler=command_handler
-        
+    '''This is the add command class'''
     def execute(self, *args):
-        if not args:  # This prompts for input, if arguments are not given
+        if not args:  # This prompts for input, if arguments are not given.
             args = input("Enter two numbers separated by space: ").split()
         if len(args) != 2:  # Makes sure that only two arguments must be given
             print("Only two arguments must be given")
@@ -19,5 +17,8 @@ class Add(Command):
             print(f"{x} + {y} = {result}")
         except InvalidOperation:
             print(f"One of the entered numbers is invalid. Please enter valid inputs.")
+        except ValueError as ve: 
+            print(f"{ve}")
         except Exception as e:
             print(f"{e}")
+            
